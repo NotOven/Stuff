6ce07cf64e9cf7f3274790f9071847404d331e9f
NotOven <97773165+NotOven@users.noreply.github.com>
2022-05-16 17:07:13 -0400

    Delete Syn.lua

--- Syn.lua
+++
@@ -1,4 +0,0 @@
-local getasset = getsynasset or getcustomasset
-
-makefolder("OvensFolder")
-writefile("OvensFolder/Bhop.mp3", game:HttpGet("https://github.com/NotOven/Audios/blob/main/bhop.mp3?raw=true"))
